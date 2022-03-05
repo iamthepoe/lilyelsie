af5ecaac14999d420921a606313afae832e903e9
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-03-05 20:17:09 -0300

    primeiro commit

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+var username = prompt("Insira seu nome: ");
+if(!username){
+	username = 'Cristiano Ronaldo';
+}
+function LilyAnswer(message){
+
+	if(message.includes(('OI'))){
+		return "Olá!";
+	}else if(message.includes(('FI'))){
+		return "Fi, você é brabo!"
+	}else if(message.includes(('TE AMO'))){
+		return "E eu te amo mais ainda, " + username + "!";
+	}else if(message.length>30){
+		return "Eu adoro quando você fala muito!";
+	}else{
+		let OtherAnswers = ["Nossa, você é demais!", "Hahaha, que bacana!",
+		"Não brinca, sério mesmo?!", "Impressionante, conte-me mais...",
+		"Isso é bem estranho, haha!"];
+		let x = Math.floor(Math.random() * OtherAnswers.length);
+		return OtherAnswers[x];
+	}
+}
+document.getElementById("btnSend").addEventListener('click', ()=>{
+	let chat = document.getElementById("chat");
+	let msg = document.getElementById("message");
+	if(msg.value){
+		chat.innerHTML += `\n`;
+		chat.innerHTML += `[${username}]: ${msg.value}\n`;
+		chat.innerHTML += `\n`;
+		chat.innerHTML += `[Lily]: ${LilyAnswer((msg.value).toUpperCase())}\n`;
+	}
+});
